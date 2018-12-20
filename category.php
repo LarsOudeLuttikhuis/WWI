43dec8c0fb768aa9972f6043751285afb599166c
annesoepboer <46020547+annesoepboer@users.noreply.github.com>
2018-12-20 13:31:39 +0100

    category

--- category.php
+++ category.php
@@ -5,11 +5,11 @@
 <div class="container">	
 	<div class="row category-child" style="margin-top:20px">
 		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
-                <a class="thumbnail" href="#">
+                <a class="thumbnail" href="nieuweartikelen.php">
                     <img class="img-responsive" src="http://kudo.co.id/wp-pictures/product/example/woven_che_white.jpg" alt="">
                     <div class="wrapper">
                         <div class="caption post-content">
-                            <span>Kebutuhan Ibu dan Anak</span>
+                            <span>nieuwe artikelen</span>
                         </div>
                     </div>
                 </a>
@@ -19,7 +19,7 @@
 					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/fashion.png" alt="">
                     <div class="wrapper">
                         <div class="caption post-content">
-                            <span>Produk Pilihan</span>        
+                            <span>Kleding</span>        
                         </div>
                     </div>
                 </a>
@@ -29,7 +29,7 @@
                     <img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/hijab.png" alt="">
                     <div class="wrapper">
                         <div class="caption post-content">
-                            <span>Produk Pilihan</span>        
+                            <span>Mokken</span>        
                         </div>
                     </div>
                 </a>
@@ -39,17 +39,57 @@
 					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/makanandaerah.png" alt="">
                     <div class="wrapper">
                         <div class="caption post-content">
-                            <span>Produk Pilihan</span>        
+                            <span>T-shirts</span>        
                         </div>
                     </div>
                 </a>
+
         </div>
+
         <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="#">
 					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/tiket.png" alt="">
                     <div class="wrapper">
                         <div class="caption post-content">
-                            <span>Produk Pilihan</span>        
+                            <span>Luchtvaart accessoires</span>        
+                        </div>
+                    </div>
+                </a>
+
+        </div>
+</div>
+
+<div class="row category-child" style="margin-top:20px">
+        <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="productoverzicht.php">
+					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Computer toebehoren</span>        
+                        </div>
+                    </div>
+                </a>
+
+        </div>
+ 
+    
+    <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>USB accessoires</span>        
+                        </div>
+                    </div>
+                </a>
+        </div>
+  
+    <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Bonten schoensel</span>        
                         </div>
                     </div>
                 </a>
@@ -59,12 +99,21 @@
 					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
                     <div class="wrapper">
                         <div class="caption post-content">
-                            <span>Produk Pilihan</span>        
+                            <span>Speelgoed</span>        
                         </div>
                     </div>
                 </a>
         </div>
-	</div>
-
+        <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Verpakkings materiaal</span>        
+                        </div>
+                    </div>
+                </a>
+        </div>
 </div>
+
 <?php include 'footer.php';?>
