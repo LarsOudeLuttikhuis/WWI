fe9ee9a988c165ea9dc7743743840f41ec9efdd3
annesoepboer <46020547+annesoepboer@users.noreply.github.com>
2018-12-20 14:39:39 +0100

    category

--- category.php
+++ category.php
@@ -6,7 +6,7 @@
 	<div class="row category-child" style="margin-top:20px">
 		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="nieuweartikelen.php">
-                    <img class="img-responsive" src="http://kudo.co.id/wp-pictures/product/example/woven_che_white.jpg" alt="">
+                <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-7.jpg">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>nieuwe artikelen</span>
@@ -16,7 +16,7 @@
         </div>
 		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="kleding.php">
-					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/fashion.png" alt="">
+                <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-7.jpg">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>Kleding</span>        
@@ -26,7 +26,7 @@
         </div> 
 		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="mokken.php">
-                    <img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/hijab.png" alt="">
+                <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-7.jpg">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>Mokken</span>        
@@ -36,7 +36,7 @@
         </div>
 		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="t-shirts.php">
-					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/makanandaerah.png" alt="">
+                <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-7.jpg">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>T-shirts</span>        
@@ -48,7 +48,7 @@
 
         <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="luchtvaartaccessoires.php">
-					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/tiket.png" alt="">
+                <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-7.jpg">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>Luchtvaart accessoires</span>        
@@ -62,7 +62,7 @@
 <div class="row category-child" style="margin-top:20px">
         <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="computertoebehoren.php">
-					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-7.jpg">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>Computer toebehoren</span>        
@@ -75,7 +75,7 @@
     
     <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="usbaccessoires.php">
-					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-7.jpg">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>USB accessoires</span>        
@@ -86,7 +86,7 @@
   
     <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="bonteschoeisel.php">
-					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-7.jpg">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>Bonte schoeisel</span>        
@@ -96,8 +96,8 @@
         </div>
         <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="speelgoed.php">
-					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
-                    <div class="wrapper">
+                <img class="pic-1" src="https://www.planethappy.nl/resize/30398_17520010732582.jpg/0/1100/True/brio-speelgoed-boerderij.jpg">
+                <div class="wrapper">
                         <div class="caption post-content">
                             <span>Speelgoed</span>        
                         </div>
@@ -106,7 +106,7 @@
         </div>
         <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
                 <a class="thumbnail" href="verpakkingsmateriaal.php">
-					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                <img src="brio-speelgoed-boerderij.jpg" alt="Italian Trulli">
                     <div class="wrapper">
                         <div class="caption post-content">
                             <span>Verpakkingsmateriaal</span>        
