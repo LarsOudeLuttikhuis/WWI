a3f8c61a4f273d2ee7ded08ebbc89a91b139cd3c
Lars Oude Luttikhuis <loudeluttikhuus@gmail.com>
2019-01-07 08:40:48 +0100

    Add contact form

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-sm-12 col-lg-12">
                 <h1 class="h1">
-                    Contact us <small>Feel free to contact us</small></h1>
+                    Contact us</h1>
             </div>
         </div>
     </div>
@@ -13,47 +13,55 @@
     <div class="row">
         <div class="col-md-8">
             <div class="well well-sm">
-                <form>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="name">
-                                Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
-                        </div>
-                        <div class="form-group">
-                            <label for="email">
-                                Email Address</label>
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
-                                </span>
-                                <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="subject">
-                                Subject</label>
-                            <select id="subject" name="subject" class="form-control" required="required">
-                                <option value="na" selected="">Choose One:</option>
-                                <option value="service">General Customer Service</option>
-                                <option value="suggestions">Suggestions</option>
-                                <option value="product">Product Support</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="name">
-                                Message</label>
-                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
-                                placeholder="Message"></textarea>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
-                            Send Message</button>
-                    </div>
-                </div>
-                </form>
+            <form name="contactform" method="post" action="send_form_email.php">
+            <table width="450px">
+            <tr>
+            <td valign="top">
+            <label for="first_name">First Name *</label>
+            </td>
+            <td valign="top">
+            <input  type="text" name="first_name" maxlength="50" size="30">
+            </td>
+            </tr>
+            <tr>
+            <td valign="top"">
+            <label for="last_name">Last Name *</label>
+            </td>
+            <td valign="top">
+            <input  type="text" name="last_name" maxlength="50" size="30">
+            </td>
+            </tr>
+            <tr>
+            <td valign="top">
+            <label for="email">Email Address *</label>
+            </td>
+            <td valign="top">
+            <input  type="text" name="email" maxlength="80" size="30">
+            </td>
+            </tr>
+            <tr>
+            <td valign="top">
+            <label for="telephone">Telephone Number</label>
+            </td>
+            <td valign="top">
+            <input  type="text" name="telephone" maxlength="30" size="30">
+            </td>
+            </tr>
+            <tr>
+            <td valign="top">
+            <label for="comments">Comments *</label>
+            </td>
+            <td valign="top">
+            <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+            </td>
+            </tr>
+            <tr>
+            <td colspan="2" style="text-align:center">
+            <input type="submit" value="Submit">
+            </td>
+            </tr>
+            </table>
+            </form>
             </div>
         </div>
         <div class="col-md-4">
