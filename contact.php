2cfc3c62af7d1694b1018caf3cac1be33c303afe
Lars Oude Luttikhuis <loudeluttikhuus@gmail.com>
2019-01-07 09:16:41 +0100

    Engels naar Nederlands

--- contact.php
+++ contact.php
@@ -14,53 +14,53 @@
         <div class="col-md-8">
             <div class="well well-sm">
             <form name="contactform" method="post" action="send_form_email.php">
-            <table width="450px">
-            <tr>
-            <td valign="top">
-            <label for="first_name">First Name *</label>
-            </td>
-            <td valign="top">
-            <input  type="text" name="first_name" maxlength="50" size="30">
-            </td>
-            </tr>
-            <tr>
-            <td valign="top"">
-            <label for="last_name">Last Name *</label>
-            </td>
-            <td valign="top">
-            <input  type="text" name="last_name" maxlength="50" size="30">
-            </td>
-            </tr>
-            <tr>
-            <td valign="top">
-            <label for="email">Email Address *</label>
-            </td>
-            <td valign="top">
-            <input  type="text" name="email" maxlength="80" size="30">
-            </td>
-            </tr>
-            <tr>
-            <td valign="top">
-            <label for="telephone">Telephone Number</label>
-            </td>
-            <td valign="top">
-            <input  type="text" name="telephone" maxlength="30" size="30">
-            </td>
-            </tr>
-            <tr>
-            <td valign="top">
-            <label for="comments">Comments *</label>
-            </td>
-            <td valign="top">
-            <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
-            </td>
-            </tr>
-            <tr>
-            <td colspan="2" style="text-align:center">
-            <input type="submit" value="Submit">
-            </td>
-            </tr>
-            </table>
+                <table width="450px">
+                    <tr>
+                    <td valign="top">
+                        <label for="first_name">Voornaam *</label>
+                    </td>
+                    <td valign="top">
+                        <input  type="text" name="first_name" maxlength="50" size="30">
+                    </td>
+                    </tr>
+                    <tr>
+                    <td valign="top"">
+                        <label for="last_name">Achternaam *</label>
+                    </td>
+                    <td valign="top">
+                        <input  type="text" name="last_name" maxlength="50" size="30">
+                    </td>
+                    </tr>
+                    <tr>
+                    <td valign="top">
+                        <label for="email">Email Adres *</label>
+                    </td>
+                    <td valign="top">
+                        <input  type="text" name="email" maxlength="80" size="30">
+                    </td>
+                    </tr>
+                    <tr>
+                    <td valign="top">
+                        <label for="telephone">Telefoon Nummer</label>
+                    </td>
+                    <td valign="top">
+                        <input  type="text" name="telephone" maxlength="30" size="30">
+                    </td>
+                    </tr>
+                    <tr>
+                    <td valign="top">
+                        <label for="comments">Opmerking *</label>
+                    </td>
+                    <td valign="top">
+                        <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+                    </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align:center">
+                        <input type="submit" value="Submit">
+                    </td>
+                    </tr>
+                </table>
             </form>
             </div>
         </div>
--- send_form_email.php
+++ send_form_email.php
@@ -9,10 +9,10 @@ if(isset($_POST['email'])) {
  
     function died($error) {
         // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
+        echo "Sorry, er is een probleem met het versturen van het formulier.";
+        echo "Deze fout melding.<br /><br />";
         echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
+        echo "Ga A.U.B. terug en probeer het optelossen.<br /><br />";
         die();
     }
  
@@ -23,7 +23,7 @@ if(isset($_POST['email'])) {
         !isset($_POST['email']) ||
         !isset($_POST['telephone']) ||
         !isset($_POST['comments'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        died('Sorry, er is een probleem met het versturen van het formulier.');       
     }
  
      
@@ -38,21 +38,21 @@ if(isset($_POST['email'])) {
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
   if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    $error_message .= 'De ingevoerde email is niet geldig.<br />';
   }
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
   if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+    $error_message .= 'De ingevoerde voornaam is niet geldig.<br />';
   }
  
   if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+    $error_message .= 'De ingevoerde achternaam is niet geldig.<br />';
   }
  
   if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+    $error_message .= 'De ingevoerde opmerking is niet geldig.<br />';
   }
  
   if(strlen($error_message) > 0) {
