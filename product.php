7692427082dec2884a395893f5f147d76672c0ca
Lars Oude Luttikhuis <loudeluttikhuus@gmail.com>
2019-01-08 19:26:05 +0100

    removed select option

--- product.php
+++ product.php
@@ -5,31 +5,5 @@ $Pid = intval($_GET['Pid']);
 ProductOverzichtBekijken($Pid);
 ToonProduct();
 sluitConnectiePDO();
-?>
-<!DOCTYPE html>
-<html>
-<body>
-<div align= "right">
-<h4>Quantity</h4>
-<form>
-  <select name="Quantity">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-    <option value="9">9</option>
-    <option value="10">10</option>
-  </select>
-  <br><br>
-  <input type="submit" value="Add to cart">
-</form>
-</div>
-</body>
-</html>
-<?php
 include 'footer.php';
 ?>
