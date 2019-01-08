1c3c0227ea23a3808e13b18c15518a2584d831cf
Lars Oude Luttikhuis <35922037+LarsOudeLuttikhuis@users.noreply.github.com>
2019-01-08 21:14:31 +0100

    Update sql.php

--- Functions/sql.php
+++ Functions/sql.php
@@ -6,7 +6,7 @@ if (!function_exists('maakConnectiePDO')) {
         function maakConnectiePDO() {
         global $connectie;
         $dsn = "mysql:host=localhost;dbname=wideworldimporters;";
-        $connectie = new PDO($dsn, 'root', 'Welkom01!');
+        $connectie = new PDO($dsn, 'root', '');
     }
 }
 
