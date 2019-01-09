c29f1123926acb804ec520ab54e97e48f8e06e43
Lars Oude Luttikhuis <35922037+LarsOudeLuttikhuis@users.noreply.github.com>
2019-01-09 12:39:10 +0100

    Revert "Lars final fix"

--- choco.php
+++ choco.php
@@ -1,7 +1,7 @@
 <?php
 include 'navbar.php';
 include_once 'Functions/global.php';
-HomeProductenOverzichtBekijken();
+HomeProductenOverzichtBekijken(1);
 $teller = 0;
 $firstTime = true;
 ?>
@@ -17,15 +17,17 @@ $firstTime = true;
         <?}
         $firstTime = false;?>
         <div class='col-md-2'>
-            <div class='product-grid'>
-                <div class='product-image'>
-                    <a href='product.php?Pid=<?echo $product["StockItemID"]?>'><img class='pic-1' src='images/240x250.png'></a>
-                </div>
-                <div class='product-content'>
-                    <h3 class='title'><a href='product.php?Pid=<?echo $product["StockItemID"]?>.php'><? echo $product["StockItemName"] ?></a></h3>
-                    <a class='btn btn-success' href='bevestiging.php?nummer=<? echo $product["StockItemID"]?>'>In Winkelwagen</a>
-                </div>
-            </div>
+        <div class='product-grid'>
+        <div class='product-image'>
+        <a href='product.php?Pid=<?echo $product["StockItemID"]?>'>
+        <img class='pic-1' src='images/240x250.png'>
+         </a>
+        </div>
+        <div class='product-content'>
+        <h3 class='title'><a href='product.php?Pid=<?echo $product["StockItemID"]?>.php'><? echo $product["StockItemName"] ?></a></h3>
+        <a class='btn btn-success' href='bevestiging.php?nummer=<? echo $product["StockItemID"]?>'>In Winkelwagen</a>
+        </div>
+        </div>
         </div>
         <?$teller += 1;
     }?>
