17f9a9f48c18ad1d2b779b811fe71cd0ff55502c
norela <danielrobbers@gmail.com>
2019-01-10 19:58:11 +0100

    Afvangen geen resultaat
    Aangepast zodat wanneer een zoekopdracht geen resultaat opleverd dit wordt getoond. Verder wat documentatie en grotere tekst.

--- search.php
+++ search.php
@@ -5,20 +5,30 @@ include_once 'navbar.php';
 
 $query = $_GET['query'];
 maakConnectiePDO();
-
+print('<h2>');
+# Afvangen van een te korte zoekterm
 if(strlen($query) <= 2){ 
     print ( 'De zoekopdracht was kleiner dan drie karakters, a.u.b. een specifiekere zoekterm gebruiken.' );
-}
-
-else if(strlen($query) >= 2){ 
+} 
+# Geval dat de zoekopdracht het minimaal aantal karakters heeft
+else { 
     $query = htmlspecialchars($query);
     global $connectie, $resultaat;
+    # sql query
     $sql = "SELECT StockItemID, StockItemName FROM stockitems WHERE StockItemName LIKE '%".$query."%'";
-        foreach ($connectie->query($sql) as $row){
-            if ($sql = true)
-         print('<a href="product.php?Pid='.$row["StockItemID"].'"> '.$row['StockItemName'].' <br>');
-    }
+    if ($res = $connectie->query($sql)) {
+        # Bepalen of de query een resultaat geeft
+        if ($res->fetchColumn() > 0) {
+            foreach ($connectie->query($sql) as $row){
+                if ($sql = true){
+                    print('<a href="product.php?Pid='.$row["StockItemID"].'"> '.$row['StockItemName'].' <br>');
+                } 
+            }
+        # Als de zoekterm geen resultaten opgeeft
+        } else {
+            print('Uw zoekopdracht heeft geen resultaten opgeleverd.');
+        }
+    } 
 }
-
 sluitConnectiePDO();
 ?>
