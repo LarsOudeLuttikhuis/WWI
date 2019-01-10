97b9e3f96d0a513d83d8ed0b125faf6410befc2a
sarg91 <46020748+sarg91@users.noreply.github.com>
2019-01-10 15:26:33 +0100

    van alles wat daniel heeft gedaan en ik

--- Betalen.php
+++ Betalen.php
@@ -1,3 +1,38 @@
-<?
-echo ("<h3>Bestelling Geplaatst</h3>");
+<?php
+include_once "./Functions/global.php";
+include 'navbar.php';
+
+if (isset($inputEmail)) {
+    $inputVoornaam = $_SESSION["Voornaam"];
+    $inputEmail = $_SESSION["Email"];
+} else {
+    $inputVoornaam = '';
+    $inputEmail = '';
+}
+
+session_unset();
+
+$_SESSION["Voornaam"] = $inputVoornaam;
+$_SESSION["Email"] = $inputEmail;
+
+
+function doorsturen(){  
+    #sleep(6);
+    header('refresh:5; url=home.php');
+}
+
+$message = "<h3>Bestelling Geplaatst. U wordt over vijf seconden naar de homepagina gestuurd.</h3>";
+
+
+#echo "<script type='text/javascript> alert('$message'); </script>";
+print($message);
+
+?>
+
+
+    
+<?php
+include 'footer.php';
+#header('refresh:5; Location: home.php');
+doorsturen();
 ?>
